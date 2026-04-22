addbbf65ecc3537a479046f3d61edcc516d60891
Opelpe <91432013+Opelpe@users.noreply.github.com>
2026-04-22 15:57:44 +0200

    Implement getCharacterById endpoint and fix PagingDataSource after mapper removal

--- data/src/main/java/com/pnow/data/character/CharacterApi.kt
+++ data/src/main/java/com/pnow/data/character/CharacterApi.kt
@@ -1,13 +1,20 @@
 package com.pnow.data.character
 
+import com.pnow.data.character.model.CharacterDTO
 import com.pnow.data.character.model.CharacterListDTO
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApi {
 
     @GET("character/")
     suspend fun getCharacters(
-        @Query("page") page: Int
+        @Query("page") page: Int,
     ): CharacterListDTO
+
+    @GET("character/{id}")
+    suspend fun getCharacterById(
+        @Path("id") id: Int,
+    ): CharacterDTO
 }
